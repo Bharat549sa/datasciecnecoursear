44db8c73a4b1399c39fc38a672eb0240e1901287
Bharat549sa <81371017+Bharat549sa@users.noreply.github.com>
2023-01-23 11:24:05 -0500

    Create infoherewhen.kt

---
+++ infoherewhen.kt
@@ -0,0 +1,54 @@
+fun main() {
+println("Is it going to rain?")
+val probability = 70
+    when {
+        probability < 40 -> println("Unlikely")
+        probability <= 80 -> println("Likely")
+        probability < 100 -> println("Yes")
+        else -> println("What?")
+    }
+}
+
+
+fun main() {
+println("Is it going to rain?")
+val probability = 70
+val text = when {
+        probability < 40 -> "Unlikely"
+        probability <= 80 -> "Likely"
+        probability < 100 -> "Yes"
+        else -> "What?"
+    }
+    println(text)
+}
+
+fun main() {
+val number = 1 // or 2, 3, 4, 5, 6
+    when (number) {
+        1 -> {
+            println("Missed hit")
+        }
+        2, 3, 4, 5 -> {
+            println("Hit with value $number")
+        }
+        6 -> {
+            println("Critical hit")
+        }
+    }
+}
+
+fun main() {
+    var value = "ABC"
+
+    println(value is String) // true
+    println(value is Int) // false
+    println(value is Boolean) // false
+    println(value is Any) // true
+
+    value = 123
+
+    println(value is String) // false
+    println(value is Int) // true
+    println(value is Boolean) // false
+    println(value is Any) // true
+}
